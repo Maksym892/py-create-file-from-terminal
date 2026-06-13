f51ac3fb76e8c507ce6a84cdafde9daa1f5e9c0f
Maksym Zhmura <_zmuramaksim@gmail.com>
2026-06-13 15:16:33 +0300

    Solution

--- app/create_file.py
+++ app/create_file.py
@@ -59,7 +59,3 @@ def write_to_file(file_path: str, lines: list) -> None:
 
         for i, line in enumerate(lines, start=1):
             file.write(f"{i} {line}\n")
-
-
-if __name__ == "__main__":
-    main()
