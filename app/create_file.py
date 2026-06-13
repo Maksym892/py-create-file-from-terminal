a39374c7edf0e2d4cd99e0d2aa0e68dcf14f2b49
Maksym Zhmura <_zmuramaksim@gmail.com>
2026-06-13 15:05:10 +0300

    Solution

--- app/create_file.py
+++ app/create_file.py
@@ -11,17 +11,14 @@ def main() -> None:
         f_index = sys.argv.index("-f")
         file_name = sys.argv[f_index + 1]
         final_path = file_name
-        print("Found file: ", file_name)
 
     if "-d" in sys.argv:
         d_index = sys.argv.index("-d")
         if "-f" in sys.argv and sys.argv.index("-f") > d_index:
             f_index = sys.argv.index("-f")
             folders = sys.argv[d_index + 1: f_index]
-            print("Folders: ", folders)
         else:
             folders = sys.argv[d_index + 1:]
-            print("Folders: ", folders)
         folder_path = os.path.join(*folders)
         os.makedirs(folder_path, exist_ok=True)
         if "-f" in sys.argv:
@@ -34,12 +31,19 @@ def main() -> None:
             if user_input == "stop":
                 break
             content_lines.append(user_input)
-        with open(final_path, "a") as file:
-            timestamp = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
-            file.write(f"{timestamp}\n")
-            for i, content in enumerate(content_lines, start=1):
-                file.write(f"{i} {content}\n")
+        write_to_file(final_path, content_lines)
 
+def write_to_file(file_path: str, lines: list) -> None:
+    file_exists = os.path.exists(file_path)
+    with open(file_path, "a") as file:
+        if file_exists:
+            file.write("\n")
+
+        timestamp = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+        file.write(f"{timestamp}\n")
+
+        for i, line in enumerate(lines, start=1):
+            file.write(f"{i} {line}\n")
 
 if __name__ == "__main__":
     main()
